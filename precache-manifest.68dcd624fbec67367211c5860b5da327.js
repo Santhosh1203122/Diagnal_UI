2e28fe4c5d990a465f5d85f0c259d3ef9697c18b
Santhosh1203122 <rm.santhosh1234@gmail.com>
2019-06-28 01:40:22 +0530

    Updates

---
+++ precache-manifest.68dcd624fbec67367211c5860b5da327.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fcf82b628672a5e2a5116eb82ede3bde",
+    "url": "/Diagnal_UI/build/index.html"
+  },
+  {
+    "revision": "f3c38055da10f8c2bc48",
+    "url": "/Diagnal_UI/build/static/css/main.6f37c306.chunk.css"
+  },
+  {
+    "revision": "fbdca0906eb614bbf501",
+    "url": "/Diagnal_UI/build/static/js/2.9cc7fb44.chunk.js"
+  },
+  {
+    "revision": "f3c38055da10f8c2bc48",
+    "url": "/Diagnal_UI/build/static/js/main.e5fe1000.chunk.js"
+  },
+  {
+    "revision": "6ab52458c5bdb97397b6",
+    "url": "/Diagnal_UI/build/static/js/runtime~main.10aeb22b.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/Diagnal_UI/build/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "d5f2a7f493e94ed267d41620eef143d9",
+    "url": "/Diagnal_UI/build/static/media/poster1.d5f2a7f4.jpg"
+  }
+]);
